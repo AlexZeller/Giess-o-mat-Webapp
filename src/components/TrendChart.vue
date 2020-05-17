a8e6cf5867299a08651f8fd0b42d93d3d5285a8a
Alexander Zeller <alexander.zeller@t-online.de>
2020-05-17 19:21:21 +0200

    first code for charts

---
+++ src/components/TrendChart.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <line-chart v-if="loaded" :chartdata="chartdata" :options="options" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import LineChart from '../components/Chart.vue';
+export default {
+  name: 'TrendChart',
+  props: {
+    sensor: {
+      type: String,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+  components: { LineChart },
+  data: () => ({
+    loaded: false,
+    Data: [],
+    chartdata: null,
+    options: {
+      scales: {
+        xAxes: [
+          {
+            type: 'time',
+            ticks: {
+              maxTicksLimit: 10,
+            },
+            time: {
+              displayFormats: {
+                minute: 'HH:mm',
+                hour: 'HH:mm',
+                day: 'dd D.M',
+              },
+            },
+          },
+        ],
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  }),
+  mounted() {
+    this.loaded = false;
+  },
+  methods: {
+    getChartData: function(sensor, hours) {
+      this.loaded = false;
+      try {
+        axios
+          .get(process.env.VUE_APP_ROOT_API + `/sensordata/${sensor}/${hours}`)
+          .then((response) => (this.Data = response.data))
+          .then(
+            () => (
+              (this.chartdata = this.formatChartData()), (this.loaded = true)
+            )
+          );
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    formatChartData: function() {
+      var dataObj = {
+        labels: this.getTimesArray(this.Data),
+        datasets: [
+          {
+            label: 'Lufttemperatur in °C',
+            fill: false,
+            data: this.getDataArray(this.Data),
+            borderColor: ['rgba(3, 107, 252)'],
+          },
+        ],
+      };
+      return dataObj;
+    },
+    getDataArray: function(Data) {
+      var Array = [];
+      Data.forEach((element) => {
+        Array.push(element[this.sensor]);
+      });
+      return Array;
+    },
+    getTimesArray: function(TempData) {
+      var TimesArray = [];
+      TempData.forEach((element) => {
+        TimesArray.push(element.timestamp);
+      });
+      return TimesArray;
+    },
+  },
+};
+</script>
--- src/views/Zeitreihen.vue
+++ src/views/Zeitreihen.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
-    Zeitreihen
+    <v-select
+      label="Sensor"
+      item-text="label"
+      item-value="id"
+      :items="sensors"
+      v-model="sensor"
+      v-on:change="getChartData"
+    >
+    </v-select>
+    <TrendChart :sensor="sensor" :label="label" ref="TrendChart" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TrendChart from '../components/TrendChart.vue';
 
 export default {
   name: 'Zeitreihen',
-  data: () => ({}),
+  components: { TrendChart },
+  data: () => ({
+    label: 'Test',
+    graph_hours: 24,
+    sensor: 'air_temp',
+    sensors: [
+      { id: 'air_temp', label: 'Lufttemperatur' },
+      { id: 'air_humid', label: 'Relative Luftfeuchtigkeit' },
+      { id: 'soil_humid', label: 'Volumetrische Bodenfeuchte' },
+      { id: 'soil_temp', label: 'Bodentemperatur' },
+      { id: 'lux', label: 'Beleuchtungsstärke' },
+      { id: 'waterlevel', label: 'Wasserstand' },
+    ],
+  }),
+  mounted() {
+    this.$refs.TrendChart.getChartData(this.sensor, this.graph_hours);
+  },
+  methods: {
+    getChartData: function() {
+      this.$refs.TrendChart.getChartData(this.sensor, this.graph_hours);
+    },
+  },
 };
 </script>
