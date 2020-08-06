68e69ae0920fd3de7b4603c6cdb0521904656afb
Alexander Zeller <alexander.zeller@t-online.de>
2020-08-06 08:16:32 +0200

    bug fixes log

--- src/views/Log.vue
+++ src/views/Log.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <v-container fluid>
-          <v-data-table
-    :headers="headers"
-    :items="log_data"
-    :items-per-page="5"
-    class="elevation-1"
-    disable-filtering
-    disable-sort
-    dense
-    hide-default-header
-  >
-      <template v-slot:item.level="{ item }">
-      <v-chip :color="getColor(item.level)" dark>{{ item.level }}</v-chip>
-    </template></v-data-table>
-
+      <v-data-table
+        :headers="headers"
+        :items="log_data"
+        :items-per-page="5"
+        class="elevation-1"
+        disable-filtering
+        disable-sort
+        dense
+        hide-default-header
+        v-if="this.log_data != ''"
+      >
+        <template v-slot:item.level="{ item }">
+          <v-chip :color="getColor(item.level)" dark>{{ item.level }}</v-chip>
+        </template>
+      </v-data-table>
+      <h4 v-if="this.log_data == ''">Could not reach API Server</h4>
     </v-container>
   </div>
 </template>
@@ -55,9 +57,10 @@ export default {
 
   },
   mounted() {
-          axios
+    axios
       .get(process.env.VUE_APP_ROOT_API + '/log')
-      .then((response) => (this.log_data = response.data));
+      .then((response) => (this.log_data = response.data))
+      .catch(this.log_data = '');
   },
 };
 </script>
